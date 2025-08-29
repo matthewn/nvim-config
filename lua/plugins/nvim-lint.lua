97eac148e8727fcaec5213e1ad63e4e90427487c
Matthew Newton <matthewn@mahnamahna.net>
2025-08-28 21:32:54 -0700

    reconfigure lint for eslint 9.x w/ config in sane location

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,5 +1,18 @@
-require('lint').linters_by_ft = {
-  javascript = {'eslint'},
+local lint = require('lint')
+
+-- Extend the eslint linter to always add --foobar
+lint.linters.eslint.args = {
+  '--config', vim.fn.expand('~/.config/eslint/eslint.config.js'),
+  '--no-color', -- defaults from here to end of table
+  '--format',
+  'json',
+  '--stdin',
+  '--stdin-filename',
+  function() return vim.api.nvim_buf_get_name(0) end,
+}
+
+lint.linters_by_ft = {
+  javascript = { 'eslint' },
 }
 
 vim.api.nvim_create_autocmd(  { 'TextChanged', 'TextChangedI', 'BufReadPost', 'BufWritePost' }, {
@@ -7,7 +20,7 @@ vim.api.nvim_create_autocmd(  { 'TextChanged', 'TextChangedI', 'BufReadPost', 'B
   callback = function()
     -- try_lint without arguments runs the linters defined in `linters_by_ft`
     -- for the current filetype
-    require('lint').try_lint()
+    lint.try_lint()
     -- You can call `try_lint` with a linter name or a list of names to always
     -- run specific linters, independent of the `linters_by_ft` configuration
     -- require('lint').try_lint('cspell')
