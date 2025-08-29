5089514c4140d84edaa1482ac0e94c0fcea2e0d3
Matthew Newton <matthewn@mahnamahna.net>
2025-08-28 21:46:37 -0700

    self-document :)

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -2,10 +2,12 @@ vim.g.barbar_auto_setup = false
 
 if vim.g.neovide then
   require('barbar').setup {
+    -- show filetype icons but no close buttons
     icons = { button = false },
   }
 else
   require('barbar').setup {
+    -- show no filetype icons or close buttons
     icons = { button = false, filetype = { enabled = false } },
   }
 end
