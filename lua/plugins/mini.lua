26be81d1ba26b34daae01116793453d6d6a28007
Matthew Newton <matthewn@mahnamahna.net>
2025-08-29 22:00:25 -0700

    enable mini.splitjoin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,7 @@
 require('mini.completion').setup()
 require('mini.git').setup()
 require('mini.icons').setup()
+require('mini.splitjoin').setup()
 require('mini.statusline').setup({ use_icons = vim.g.neovide and true or false })
 
 
