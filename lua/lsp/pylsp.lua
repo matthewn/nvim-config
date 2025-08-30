1b4737ecea91b0df544fb93db15efaea38bd2ed2
Matthew Newton <matthewn@berkeley.edu>
2025-08-29 17:32:42 -0700

    restart LSP after direnv (so pylsp will finally behave!)

--- lua/lsp/pylsp.lua
+++ lua/lsp/pylsp.lua
@@ -1,5 +1,6 @@
 vim.lsp.config('pylsp', {
   settings = {
+    autostart = false, -- will start AFTER direnv is done
     pylsp = {
       configurationSources = { 'flake8' }, -- use ~/.config/flake8
       plugins = {
@@ -17,3 +18,19 @@ vim.lsp.config('pylsp', {
 })
 
 vim.lsp.enable('pylsp')
+
+-- restart the LSP after direnv is done
+local augroup = vim.api.nvim_create_augroup('lsp-restart', { clear = true })
+vim.api.nvim_create_autocmd('User', {
+  group = augroup,
+  pattern = 'DirenvLoaded',
+  callback = function()
+    -- small delay to ensure environment is fully loaded
+    vim.defer_fn(function()
+      -- only restart if we have LSP clients attached
+      if next(vim.lsp.get_clients()) then
+        vim.cmd("LspRestart")
+      end
+    end, 100)
+  end,
+})
