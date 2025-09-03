f750608ad42ea7c0651d1d809cee2456be95580b
Matthew Newton <matthewn@mahnamahna.net>
2025-09-02 20:16:54 -0700

    add treesitter for htmldjango

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'python' },
+  ensure_installed = { 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'python', 'htmldjango' },
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
