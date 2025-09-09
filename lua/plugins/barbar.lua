0e75d166a94c7ec6f082c43272ad7fe934765de7
Matthew Newton <matthewn@mahnamahna.net>
2025-09-08 19:23:23 -0700

    shrink some code (no logic changes)

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,13 +1,5 @@
 vim.g.barbar_auto_setup = false
-
-if vim.g.neovide then
-  require('barbar').setup {
-    -- show filetype icons but no close buttons
-    icons = { button = false },
-  }
-else
-  require('barbar').setup {
-    -- show no filetype icons or close buttons
-    icons = { button = false, filetype = { enabled = false } },
-  }
-end
+require('barbar').setup {
+  -- show filetype icons if gui; no close buttons ever
+  icons = { button = false, filetype = { enabled = vim.g.neovide and true or false } },
+}
