e0dbc7ebd089fd7f256404a7fb90fd439faaed57
Matthew Newton <matthewn@mahnamahna.net>
2025-09-09 22:02:05 -0700

    refine todo-comments config

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,13 +1,14 @@
 require('todo-comments').setup({
+  -- let's see them right here:
+  -- TODO stuff
+  -- FIXME: more stuff
   signs = false, -- no icons in the signs column
-  -- TODO things
-  -- FIXME more things
   highlight = {
     multiline = false,
     before = '',
     keyword = 'bg',
     after = '',
-    pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+    pattern = [[ (KEYWORDS):? ]], -- pattern or table of patterns, used for highlighting (vim regex)
     comments_only = true, -- uses treesitter to match keywords in comments only
     exclude = {}, -- list of file types to exclude highlighting
   },
