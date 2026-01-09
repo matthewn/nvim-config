c21efe8a83fb76613c3caa4a7e6bb1b353d22d90
Matthew Newton <matthewn@mahnamahna.net>
2026-01-08 20:59:32 -0800

    tweak goyo config again

--- lua/plugins/goyo.lua
+++ lua/plugins/goyo.lua
@@ -1,36 +1,28 @@
 vim.g.goyo_height = "90%"
 local M = {}
 M.active = false
-M.prev_colorscheme = nil
 
 vim.api.nvim_create_autocmd('User', {
   pattern = 'GoyoEnter',
   callback = function()
-    if M.active then
-      return
-    end
+    if M.active then return end
     M.active = true
-    M.prev_colorscheme = vim.g.colors_name
+
+    -- vim.cmd("colorscheme darkblue")
     vim.cmd('Bigger')
     vim.cmd('Bigger')
     vim.opt.lbr = true
-    -- vim.cmd("colorscheme darkblue")
   end,
 })
 
 vim.api.nvim_create_autocmd('User', {
   pattern = 'GoyoLeave',
   callback = function()
-    if not M.active then
-      return
-    end
+    if not M.active then return end
     M.active = false
+
     vim.cmd('Smaller')
     vim.cmd('Smaller')
     vim.opt.lbr = false
-    if M.prev_colorscheme then
-      vim.cmd('colorscheme ' .. M.prev_colorscheme)
-      M.prev_colorscheme = nil
-    end
   end,
 })
